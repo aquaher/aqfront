2a0392a138d9a74ea4590914098d97725a82dcbc
DanyXavier <d-xavi07@hotmail.com>
2022-05-01 22:06:23 -0500

    instance

---
+++ src/service/instance.jsx
@@ -0,0 +1,20 @@
+//import { session } from '@/reducer/session';
+import axios from 'axios';
+import cookie from './cookie';
+//import { useSelector } from 'react-redux';
+
+function Axios () {
+    const instance = axios.create({
+        baseURL: import.meta.env.VITE_BASE_API
+    })
+    instance.interceptors.request.use(async (request) => {
+        const token = await cookie.getCookie(import.meta.env.VITE_COOKIE_TOKEN)
+        
+        if (token) {
+            request.headers.Authorization = `Bearer ${token}`;
+        }
+        return request;
+    });
+    return instance;
+}
+export const instance = Axios();
