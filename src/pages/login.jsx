271b5ea905d43034d5f0a0fbb7875e3482aa9799
DanyXavier <d-xavi07@hotmail.com>
2022-05-12 16:30:41 -0500

    P

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,21 +1,38 @@
 import { useState } from 'react'
-import { LoginForm, LoginPage, LoginMainFooterBandItem } from '@patternfly/react-core'
-import ExclamationCircleIcon from '@patternfly/react-icons/dist/esm/icons/exclamation-circle-icon';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
 import img from '@/assets/img/b.png'
 import logo from '@/assets/img/l_aquaher.png'
 import { useAuth } from '@/auth/AuthProvider';
 import { AlertSwal } from '@/service/sweetAlert';
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://www.grupoheredia.com.ec">
+        Departemento de Tics GRUPOHEREDIA
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
 function Login() {
   const { signIn, error, session } = useAuth();
-  const [validate, setValidate] = useState({
-    isValidUsername: true,
-    isValidPassword: true,
-    isRememberMeChecked: false,
-    showHelperText: false,
-    helperText: ''
-  })
-  const [password, setPassword] = useState('');
-  const [username, setUsername] = useState('');
+
   function recuerdame(e) { }
   async function login(e) {
     e.preventDefault();
@@ -29,53 +46,91 @@ function Login() {
       },
     })
   }
-  function handleChangeUsername(e) {
-    setUsername(e)
-  }
-  function handleChangePassword(e) {
-    setPassword(e)
-  }
-  const Formulario = (
-    <LoginForm
-      showHelperText={validate.showHelperText}
-      helperText={validate.helperText}
-      helperTextIcon={<ExclamationCircleIcon />}
-      usernameLabel="Nombre de usuario"
-      usernameValue={username}
-      onChangeUsername={handleChangeUsername}
-      isValidUsername={validate.isValidUsername}
-      passwordLabel="Contraseña"
-      passwordValue={password}
-      onChangePassword={handleChangePassword}
-      isValidPassword={validate.isValidPassword}
-      rememberMeLabel="Recuerdame."
-      isRememberMeChecked={validate.isRememberMeChecked}
-      onChangeRememberMe={e => setValidate({ ...validate, isRememberMeChecked: e })}
-      onLoginButtonClick={login}
-      loginButtonLabel="Iniciar sesion"
-    />
-  );
-  const forgotCredentials = (
-    <LoginMainFooterBandItem>
-      <a href="#">Perdiste la contraseña?</a>
-    </LoginMainFooterBandItem>
-  );
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+      AlertSwal.fire({
+        title: '¡Iniciando sesión!',
+        timerProgressBar: true,
+        didOpen: async () => {
+          AlertSwal.showLoading();
+          await signIn(data.get('email'), data.get('password'));
+            AlertSwal.close();
+        },
+      })
+  };
+  
+  
   return (
-    <>
-      <LoginPage
-        footerListVariants="inline"
-        brandImgSrc={logo}
-        brandImgAlt="PatternFly logo"
-        backgroundImgSrc={img}
-
-        backgroundImgAlt="Images"
-        loginTitle="Inicia sesión con tu cuenta"
-        loginSubtitle="Ingresa aqui tus credenciales"
-        forgotCredentials={forgotCredentials}
-      >
-        {Formulario}
-      </LoginPage>
-    </>
+    <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: `url(${img})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Inicia Sesión
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Nombre de usuario"
+                name="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Contraseña"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Recuerdame"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Iniciar sesión
+              </Button>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
   )
 }
 
