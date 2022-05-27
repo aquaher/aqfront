a16200ab7da6730e66e57ec0db05a97c5aa79dd0
DanyXavier <d-xavi07@hotmail.com>
2022-05-26 20:45:10 -0500

    P

--- src/api/user.jsx
+++ src/api/user.jsx
@@ -3,7 +3,10 @@ import { instance, instanceAdmin } from "@/service/instance";
 const path = '/users';
 const getUsersFetcher = url =>instance.get(path + url).then(res => res.data)
 const getUsersKeycloak = url => instanceAdmin.get(url).then(res=>res.data)
+const deleteUsersKeycloak = url => instanceAdmin.delete(url).then(res=>res.data)
 const setUserKeycloak = (url,data) => instanceAdmin.post(url,data).then(res=>res.data)
+const getUserByUsernameKeycloak = (url,data) => instanceAdmin.get(url,{params:{exact:true,username:data}}).then(res=>res.data)
+const getGroupsKeycloak = url => instanceAdmin.get(url).then(res=>res.data);
 async function getUsers() {
     const { data } = await instance.get(path);
     return data;
@@ -19,5 +22,8 @@ export {
     getUsers,
     getUsersFetcher,
     getUsersKeycloak,
-    setUserKeycloak
+    setUserKeycloak,
+    getGroupsKeycloak,
+    getUserByUsernameKeycloak,
+    deleteUsersKeycloak
 }
--- src/pages/modules/sistema/usuarios.jsx
+++ src/pages/modules/sistema/usuarios.jsx
@@ -1,23 +1,93 @@
-import { getUsersFetcher, getUsersKeycloak, setUserKeycloak } from "@/api/user";
+import { getUsersFetcher, getUsersKeycloak, setUserKeycloak, getGroupsKeycloak,getUserByUsernameKeycloak,deleteUsersKeycloak } from "@/api/user";
 import { Edit, Password, PersonAdd, PersonAddDisabled, PersonSearch } from "@mui/icons-material";
-import { Box, Button, Chip, Dialog, DialogContent, DialogTitle, FormControlLabel, FormGroup, IconButton, Paper, Skeleton, Stack, Switch, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Tooltip, Typography } from "@mui/material";
+import { Box, Button, Chip, Dialog, DialogContent, DialogTitle, FormControl, FormControlLabel, FormGroup, FormLabel, IconButton, MenuItem, Paper, Select, Skeleton, Stack, Switch, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Tooltip, Typography } from "@mui/material";
 import { useState } from "react";
 import useSWR from "swr"
+import { Formik, Form, useFormik } from 'formik';
+
+const Formulario = ({groups}) => {
+    const formik = useFormik({
+        initialValues: {
+            username: '',
+            enabled: false,
+            emailVerified: true,
+            firstName: '',
+            lastName: '',
+            email: '',
+            groups:'selecciona un grupo'
+        },
+        onSubmit: handleSubmit,
+    });
+    async function handleSubmit(value){
+        let newVal = {...value,groups:[value.groups]}
+        try {
+            await setUserKeycloak('/users',newVal)
+            console.log('creado')
+            const up = await getUserByUsernameKeycloak('/users',value.username);
+            console.log(up)
+            const p = await getUsersKeycloak(`/users/${up[0].id}/groups`);
+            console.log(p)
+            ///groups/{groupId} delete
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    return (
+        <form onSubmit={formik.handleSubmit}>
+            <Typography>Nombre de usaurio</Typography>
+            <TextField
+                id="username"
+                name="username"
+                value={formik.values.username}
+                onChange={formik.handleChange}
+            />
+            <Typography>Nombres</Typography>
+            <TextField
+                id="firstName"
+                name="firstName"
+                value={formik.values.firstName}
+                onChange={formik.handleChange}
+            />
+            <Typography>Apellidos</Typography>
+            <TextField
+                id="lastName"
+                name="lastName"
+                value={formik.values.lastName}
+                onChange={formik.handleChange}
+            />
+            <Typography>Corre electrónico</Typography>
+            <TextField
+                id="email"
+                name="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+            />
+            <Typography>Corre electrónico</Typography>
+            <Select id="groups" name="groups" value={formik.values.groups} onChange={formik.handleChange}>
+                <MenuItem value={'selecciona un grupo'}>selecciona un grupo</MenuItem>
+                {groups.map((d,idx)=><MenuItem value={d.name} key={idx}>{d.name}</MenuItem>)}
+            </Select>
+            <FormGroup>
+                <FormControlLabel control={<Switch id="enabled" name="enabled" value={formik.values.enabled} onChange={formik.handleChange}/>} label='Verificar correo'/>
+            </FormGroup>
+            <Button variant="contained" type="submit">
+                Guardar
+            </Button>
+        </form>
+    );
+}
 
 export default function Susuarios() {
-    const { data, error } = useSWR('/users', getUsersKeycloak);
-    const [user, setUser] = useState({
-        username: '',
-        enabled: false,
-        emailVerified: true,
-        firstName: '',
-        lastName: '',
-        email: '',
-        groups:[],
-    });
+    const { data: users, error } = useSWR('/users', getUsersKeycloak);
+    const { data: groups } = useSWR('/groups', getGroupsKeycloak);
+    const [group, setGroup] = useState('selecciona un grupo');
+
+
+    //realmRoles
+    console.log(groups)
     const [open, setOpen] = useState(false);
     if (error) return <Stack>Recarga la página</Stack>
-    if (!data) return (
+    if (!users) return (
         <>
             <Skeleton />
             <Skeleton />
@@ -25,11 +95,16 @@ export default function Susuarios() {
             <Skeleton />
         </>
     );
+
+
     async function createUser(e) {
-        console.log(user)
-        const prueba = await getUsersKeycloak('/users');
-        console.log(prueba)
+
+        console.log(e)
+        //const prueba = await getUsersKeycloak('/users');
+        //console.log(prueba)
     }
+
+//delete /users/{id}
     return (
         <>
             <Stack spacing={2}>
@@ -53,42 +128,50 @@ export default function Susuarios() {
                                     </TableRow>
                                 </TableHead>
                                 <TableBody>
-                                    {data.map((e, idx) => (
+                                    {users.map((e, idx) => (
                                         <TableRow key={idx} >
                                             <TableCell>{e.username || ''}</TableCell>
                                             <TableCell>{e.lastName || ''}</TableCell>
                                             <TableCell>{e.firstName || ''}</TableCell>
-                                            <TableCell>{e.enabled?
-                                            <Chip label='Activo' color='success' size="small"/>
-                                            :<Chip label='Inactivo' color='error' size="small"/>}</TableCell>
+                                            <TableCell>{e.enabled ?
+                                                <Chip label='Activo' color='success' size="small" />
+                                                : <Chip label='Inactivo' color='error' size="small" />}</TableCell>
                                             <TableCell>{e.email || ''}</TableCell>
-                                            <TableCell>{e.emailVerified?
-                                            <Chip label='Verificado' color='success' size="small"/>
-                                            :<Chip label='No verificado' color='error' size="small"/>}</TableCell>
+                                            <TableCell>{e.emailVerified ?
+                                                <Chip label='Verificado' color='success' size="small" />
+                                                : <Chip label='No verificado' color='error' size="small" />}</TableCell>
                                             <TableCell>{new Date(e.createdTimestamp).toLocaleDateString() || ''}</TableCell>
                                             <TableCell>
                                                 <Stack direction='row' spacing={1}>
                                                     <Tooltip title='Editar'>
                                                         <IconButton>
-                                                            <Edit color="warning"/>
+                                                            <Edit color="warning" />
                                                         </IconButton>
                                                     </Tooltip>
-                                                    <Tooltip title={e.enabled?'Desactivar':'Activar'}>
+                                                    <Tooltip title={e.enabled ? 'Desactivar' : 'Activar'}>
                                                         <IconButton>
-                                                            <PersonAddDisabled color={e.enabled?'error':'success'}/>
+                                                            <PersonAddDisabled color={e.enabled ? 'error' : 'success'} />
                                                         </IconButton>
                                                     </Tooltip>
-                                                        <IconButton disabled={e.emailVerified}>
-                                                            <PersonAddDisabled/>
-                                                        </IconButton>
+                                                    <IconButton disabled={e.emailVerified}>
+                                                        <PersonAddDisabled />
+                                                    </IconButton>
                                                     <Tooltip title="Contraseña">
                                                         <IconButton >
-                                                            <Password color="primary"/>
+                                                            <Password color="primary" />
                                                         </IconButton>
                                                     </Tooltip>
                                                     <Tooltip title="Ligar acceso un empleado">
-                                                        <IconButton >
-                                                            <PersonSearch color="info"/>
+                                                        <IconButton onClick={async(v)=>{
+                                                            try {
+                                                                console.log(e.id)
+                                                                await deleteUsersKeycloak(`/users/${e.id}`);
+                                                            } catch (error) {
+                                                                
+                                                            }
+                                                            
+                                                        }}>
+                                                            <PersonSearch color="info" />
                                                         </IconButton>
                                                     </Tooltip>
                                                 </Stack>
@@ -105,18 +188,7 @@ export default function Susuarios() {
                 <DialogTitle>Creación de nuevo usuario</DialogTitle>
                 <DialogContent>
                     <Stack spacing={1}>
-                        <Typography>Nombre de usuario</Typography>
-                        <TextField value={user.username} onChange={e=>setUser(prev=>({...prev,username:e.target.value}))}/>
-                        <Typography>Nombres</Typography>
-                        <TextField value={user.firstName} onChange={e=>setUser(prev=>({...prev,firstName:e.target.value}))}/>
-                        <Typography>Apellidos</Typography>
-                        <TextField value={user.lastName} onChange={e=>setUser(prev=>({...prev,lastName:e.target.value}))}/>
-                        <Typography>Correo</Typography>
-                        <TextField value={user.email} onChange={e=>setUser(prev=>({...prev,email:e.target.value}))} />
-                        <FormGroup>
-                            <FormControlLabel control={<Switch value={user.emailVerified} defaultChecked onChange={e=>setUser(user=>({...user,emailVerified:e.target.checked}))}/>} label='Verificar correo'/>
-                        </FormGroup>
-                        <Button variant='contained' onClick={createUser}>Guardar</Button>
+                        <Formulario groups={groups} />
                     </Stack>
                 </DialogContent>
             </Dialog>
@@ -124,7 +196,23 @@ export default function Susuarios() {
     );
 }
 /*
-
+<Typography>Nombre de usuario</Typography>
+                        <TextField value={user.username} onChange={e=>setUser(prev=>({...prev,username:e.target.value}))}/>
+                        <Typography>Nombres</Typography>
+                        <TextField value={user.firstName} onChange={e=>setUser(prev=>({...prev,firstName:e.target.value}))}/>
+                        <Typography>Apellidos</Typography>
+                        <TextField value={user.lastName} onChange={e=>setUser(prev=>({...prev,lastName:e.target.value}))}/>
+                        <Typography>Correo</Typography>
+                        <TextField value={user.email} onChange={e=>setUser(prev=>({...prev,email:e.target.value}))} />
+                        <Typography>Grupo</Typography>
+                        <Select value={group} onChange={(e)=>setGroup(e.target.value)}>
+                            <MenuItem value={'selecciona un grupo'}>selecciona un grupo</MenuItem>
+                            {groups.map((d,idx)=><MenuItem value={d.name} key={idx}>{d.name}</MenuItem>)}
+                        </Select>
+                        <FormGroup>
+                            <FormControlLabel control={<Switch value={user.emailVerified} defaultChecked onChange={e=>setUser(user=>({...user,emailVerified:e.target.checked}))}/>} label='Verificar correo'/>
+                        </FormGroup>
+                        <Button variant='contained' onClick={createUser}>Guardar</Button>
 {
         "createdTimestamp": 1588880747548,
         "username": "Strange",
