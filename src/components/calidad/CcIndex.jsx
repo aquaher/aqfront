6dda26acdbaa93f6b2d71944f86e237d3f16af6e
aquaher <sistemas@grupoheredia.com.ec>
2023-05-11 12:41:00 -0500

    s

--- src/components/calidad/CcIndex.jsx
+++ src/components/calidad/CcIndex.jsx
@@ -237,7 +237,7 @@ const ComponentPrint = forwardRef((props, ref) => {
                 <Stack direction='row' justifyContent='space-between' mt={2}>
                     <Stack>
                         <Typography fontWeight='bold'>
-                            Ing. Qca. Veronica Vinces
+                            Ing. Qca. Veronica Vinces 
                         </Typography>
                         <Typography fontWeight='bold'>
                             Control de Calidad
